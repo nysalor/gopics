7a9073cfdc6aae4104d06d0feca0427e13c7ba58
Jun Yokoyama <jun@larus.org>
2018-09-11 18:50:07 +0900

    albumsテーブルのimages_countにデフォルト値を設定し、imagesテーブルにcreated_at,updated_atを追加した

--- initial.sql
+++ initial.sql
@@ -1,3 +1,3 @@
 create database if not exists gopics;
-create table if not exists gopics.albums (id integer NOT NULL AUTO_INCREMENT, name varchar(255), dirname varchar(255), images_count integer, updated_at datetime, created_at datetime, primary key(id));
-create table if not exists gopics.images (id integer NOT NULL AUTO_INCREMENT, album_id integer, filename varchar(255), maker varchar(255), model varchar(255), lens_maker varchar(255), lens_model varchar(255), took_at datetime, f_number float, focal_length integer, iso integer, latitude float, longitude float, primary key(id));
+create table if not exists gopics.albums (id integer NOT NULL AUTO_INCREMENT, name varchar(255), dirname varchar(255), images_count integer default 0, updated_at datetime, created_at datetime, primary key(id));
+create table if not exists gopics.images (id integer NOT NULL AUTO_INCREMENT, album_id integer, filename varchar(255), maker varchar(255), model varchar(255), lens_maker varchar(255), lens_model varchar(255), took_at datetime, f_number float, focal_length integer, iso integer, latitude float, longitude float, updated_at datetime, created_at datetime, primary key(id));
